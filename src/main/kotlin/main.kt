8b8bea46f5cc7b2fe9e9a1a545e657380d9aa6a8
robomajor <robokajor@gmail.com>
2021-02-22 22:21:10 +0100

    Class about Set

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,4 +9,18 @@ fun main() {
             i -> println("Some useful info on user $i")
     }
     // getSysSudoers().add(5) <- Error!           //Attempting to write to the read-only view causes a compilation error.
+
+
+    val aboutSet = AboutSet()
+
+    val aNewIssue = "uniqueDescr4"
+    val anIssueAlreadyIn = "uniqueDescr2"
+
+    println("Issue $aNewIssue ${aboutSet.getStatusLog(aboutSet.addIssue(aNewIssue))}")
+    //Prints a success message: the new element is added to the Set.
+
+    println("Issue $anIssueAlreadyIn ${aboutSet.getStatusLog(aboutSet.addIssue(anIssueAlreadyIn))}")
+    //Prints a failure message: the element can't be added because it duplicates an existing element.
+
+    println(aboutSet.getIssuesSet())
 }
